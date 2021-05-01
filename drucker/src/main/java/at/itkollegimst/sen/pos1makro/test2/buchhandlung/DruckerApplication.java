befe6633d2252fa524f50f1cd09effb96d8c5360
Tayfun Sen <sen.ghost@outlook.com>
2021-05-01 22:34:35 +0200

    Mein erster Commit, mit die 3 MS und ihre application properties

---
+++ drucker/src/main/java/at/itkollegimst/sen/pos1makro/test2/buchhandlung/DruckerApplication.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.sen.pos1makro.test2.buchhandlung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruckerApplication.class, args);
+    }
+
+}
